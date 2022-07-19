a918a3e6e0f66d948bf48615771a59de1e80a522
sonuv2201 <sonuv.2201@gmail.com>
2022-07-19 20:55:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import ProductItem from "./ProductItem";
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 function App() {
+  const [data,setData] = useState([]);
+  const [loading,setLoading] = useState(false);
+
+  useEffect(()=>{
+    setLoading(true);
+    fetch('https://dummyjson.com/products').then((response)=> response.json() ).then(res=>{
+      setData(res.products);
+      setLoading(false);
+    });
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h2>Product Data</h2> 
+        <div className="wrapper">
+          {
+            loading ? <Display_dummy /> : data.map((item)=><ProductItem porductData={item} key={item.id} />)
+          }
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+const Display_dummy =()=>{
+  return(
+    <>
+    <div>
+    <Skeleton height={200}/> <br />
+    <Skeleton /><br />
+    <Skeleton height={50}/> <br />
+    <Skeleton height={30}/> 
+    </div> 
+
+    <div>
+    <Skeleton height={200}/> <br />
+    <Skeleton /><br />
+    <Skeleton height={50}/> <br />
+    <Skeleton height={30}/> <br />
+    </div> 
+
+    <div>
+    <Skeleton height={200}/> <br />
+    <Skeleton /><br />
+    <Skeleton height={50}/> <br />
+    <Skeleton height={30}/> 
+    </div> 
+
+
+    <div>
+    <Skeleton height={200}/> <br />
+    <Skeleton /><br />
+    <Skeleton height={50}/> <br />
+    <Skeleton height={30}/> 
+    </div> 
+
+    <div>
+    <Skeleton height={200}/> <br />
+    <Skeleton /><br />
+    <Skeleton height={50}/> <br />
+    <Skeleton height={30}/> 
+    </div> 
+
+    <div>
+    <Skeleton height={200}/> <br />
+    <Skeleton /><br />
+    <Skeleton height={50}/> <br />
+    <Skeleton height={30}/> 
+    </div> 
+
+    <div>
+    <Skeleton height={200}/> <br />
+    <Skeleton /><br />
+    <Skeleton height={50}/> <br />
+    <Skeleton height={30}/> 
+    </div> 
+
+    <div>
+    <Skeleton height={200}/> <br />
+    <Skeleton /><br />
+    <Skeleton height={50}/> <br />
+    <Skeleton height={30}/> 
+    </div> 
+    </>
+  )
+}
